e34daa1bff9107ca58c44b8bd539481fceea045f
denzio231 <108673932+denzio231@users.noreply.github.com>
2022-12-05 23:06:46 +0800

    Update Day 2.py

--- Day 2.py
+++ Day 2.py
@@ -1,3 +1,4 @@
+
 def convert_add(list_num):
     to_return = 0
     for num in list_num:
@@ -7,21 +8,27 @@ def convert_add(list_num):
 
 
 def check_duplicates(list_str):
+    flag =  False
+    l = set()
     i = 1
     r = 1
     for items in list_str:
         
         for robins in list_str:
             if items == robins and i != r:
-                return items
+                flag = True
+                l.add(items)
             r += 1
         r = 1
         i += 1
-    return 'No duplicates'
+    if flag:
+        return l
+    else:
+        return 'No duplicates'
         
     
     
         
     
 print(convert_add(['1','2']))
-print(check_duplicates(['lol','lo']))
+print(check_duplicates( ['apple', 'apple', 'peach', 'peach', 'banana']))
