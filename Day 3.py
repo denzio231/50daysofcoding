1cb62e2b7871295fc5cc8d12c66187c7cd18475f
denzio231 <108673932+denzio231@users.noreply.github.com>
2022-12-06 15:00:11 +0800

    Update Day 3.py

--- Day 3.py
+++ Day 3.py
@@ -4,13 +4,14 @@ def register_check(dictionary):
         if v.lower() == 'yes':
             counter += 1
     return counter
-
+print(register_check({'Michael' : 'yes', 'John' : 'no' , 'Peter' : 'Yes', 'Mary':'Yes'}))
+            
 def lowercase(l):
     to_return = []
     for items in l:
         if items.lower() == items:
             to_return.append(items)
-            print(to_return)
     return tuple(to_return)
             
             
+print(lowercase( ["kerry", "dickson", "John", "Mary", "carol", "Rose", "adam"]))
