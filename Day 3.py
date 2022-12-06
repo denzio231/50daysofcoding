2dd9de6aff74471c3d5d0072696eb143169ac078
denzio231 <108673932+denzio231@users.noreply.github.com>
2022-12-06 14:34:22 +0800

    Add files via upload

---
+++ Day 3.py
@@ -0,0 +1,18 @@
+def register_check(dictionary):
+    counter = 0
+    for k,v in dictionary.items():
+        if v.lower() == 'yes':
+            counter += 1
+    return counter
+print(register_check({'Michael' : 'yes', 'John' : 'no' , 'Peter' : 'Yes', 'Mary':'Yes'}))
+            
+def lowercase(l):
+    to_return = []
+    for items in l:
+        if items.lower() == items:
+            to_return.append(items)
+            print(to_return)
+    return tuple(to_return)
+            
+            
+print(lowercase( ["kerry", "dickson", "John", "Mary", "carol", "Rose", "adam"]))
