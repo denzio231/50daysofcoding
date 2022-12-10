08adf4081cc7b1283b1649fd3a08ef394803db12
denzio231 <108673932+denzio231@users.noreply.github.com>
2022-12-10 20:50:46 +0800

    Create Day 7.py

---
+++ Day 7.py
@@ -0,0 +1,15 @@
+def str_range(num):
+	return str(list(range(0,num))).replace(',','.').replace('[','').replace(']','')
+	
+def nameswiths(names):
+	to_return = {}
+	for name in names:
+		
+		if name[0].lower() == "s" and name not in to_return:
+			to_return[name]= 1
+		elif name[0].lower() == "s":
+			to_return[name] += 1
+	return to_return
+		
+print(str_range(5))
+print(nameswiths(["Adam","Sasha","Wopal","Sala","Sala"]))
